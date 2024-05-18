18b474e048c75f027dd62b75aa82cd32ceaa2183
Rhoshan <rhoshan02@gmail.com>
2024-05-18 02:50:15 +0100

    feat(signup funtion started)

--- run.py
+++ run.py
@@ -12,8 +12,37 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project-3')
 
-sales = SHEET.worksheet('Battlefield')
+def validate_email_input(email):
+    return "@" in email and email.endswith(".com")
 
-data = sales.get_all_values()
 
-print(data)
+def sign_up(pEmail,pPassword):
+    """
+    Checks if the email and password exists
+    """
+    all_user_accounts = SHEET.worksheet("Users").get_all_values()
+    found = False
+    for emails , passwords in all_user_accounts:
+        if pEmail == emails and pPassword == passwords:
+            print(f'Logged in successfully , welcome {pEmail}')
+            found = True
+            break
+    if not found:
+        print('Your account was not found , would you like to signup ?')
+
+
+def main():
+    """
+    Run all program functions
+    """
+    user_email = input('Enter Email: ')
+    user_password = input('Enter Password: ')
+
+    valid = validate_email_input(user_email)
+    if valid and user_password != '':
+        sign_up(user_email,user_password)
+    else:
+        print('Please provide a valid email and password !')
+
+print("Welcome To Rhoshans Login Management System Please Follow Instructions On Screen!")
+main ()
